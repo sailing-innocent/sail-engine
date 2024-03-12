1eb3785f3c6845ca3df3fca83a823a1ad962d63b
unknown <sailing-innocent>
2024-03-12 16:58:36 +0800

    feat: rerun se_test

--- xmake.lua
+++ xmake.lua
@@ -6,13 +6,10 @@ engine_version = "0.1.0"
 
 -- default options
 includes("build_files/xmake/default_options.lua")
--- try generated options
-includes("build_files/xmake/_options.lua")
 -- rules 
 includes("build_files/xmake/rules.lua")
 
 -- modules
 includes("external") -- external dependencies
 includes("modules") -- core engine
-includes("internal") -- internal modules
--- includes("tests") -- tests
+includes("tests")
