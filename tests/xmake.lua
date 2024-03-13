ae624dbdc05c53f2d8a1660eaffce8745d7db84c
unknown <sailing-innocent>
2024-03-13 16:37:23 +0800

    feat: add learn ogl test case

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -19,5 +19,5 @@ if get_config("enable_inno") then
 end 
 if get_config("enable_ing") then 
     sail_add_test("ing", "util", {"SailIng"})
-    sail_add_test("ing", "learn_ogl", {"SailIng"})
+    sail_add_test("ing", "learn_ogl", {"SailIng", "shaders", "textures"})
 end 
