8279021bc4ce6c9bc4afe28f37fe4a053bd31261
sailing-innocent <colored_black@outlook.com>
2024-03-26 15:50:02 +0800

    fix: mkdir

--- cent/lib/reimpl/vanilla_diff_gaussian/scene/gaussian_model.py
+++ cent/lib/reimpl/vanilla_diff_gaussian/scene/gaussian_model.py
@@ -188,7 +188,7 @@ class GaussianModel:
         return l
 
     def save_ply(self, path):
-        os.mkdir(os.path.dirname(path), exists_ok=True)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         xyz = self._xyz.detach().cpu().numpy()
         normals = np.zeros_like(xyz)
         f_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
