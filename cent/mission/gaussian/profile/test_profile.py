3173685842c511d8e3c3c9312305d17dfcaa709c
sailing-innocent <colored_black@outlook.com>
2024-03-26 17:48:51 +0800

    train full

--- cent/mission/gaussian/profile/test_profile.py
+++ cent/mission/gaussian/profile/test_profile.py
@@ -2,8 +2,8 @@ from .mission import Mission
 
 
 def test_profile():
-    # f = "profile_vanilla_train"
-    f = "profile_vanilla_eval"
+    f = "profile_vanilla_train"
+    # f = "profile_vanilla_eval"
     m = Mission(f + ".json")
     m.exec()
     # assert True 
