d3852d40fa9eeaa71457b9acfc5e2d06514fe655
sailing-innocent <colored_black@outlook.com>
2024-04-02 15:18:02 +0800

    sync

--- cent/mission/gaussian/split/test_split.py
+++ cent/mission/gaussian/split/test_split.py
@@ -1,2 +1,11 @@
+from module.model.gaussian.zzh import GaussianModel
+from module.dataset.nvs.blender.dataset import create_dataset as create_nerf_blender_dataset
+from mission.config.env import get_env_config
+
 def test_split():
-    assert True 
+    gs = GaussianModel(3)
+    env_config = get_env_config()
+    dataset = create_nerf_blender_dataset(env_config, 'lego', 'train')
+    pcd = dataset.get_point_cloud()
+    gs.create_from_pcd(pcd, 1.0)
+    gs.save_ply('D:/pretrained_random.ply')
