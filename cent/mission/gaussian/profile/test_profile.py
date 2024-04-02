89e49ee74c7b3c6d6aa107d0d7f8c676b38d2cfe
sailing-innocent <colored_black@outlook.com>
2024-04-02 22:09:15 +0800

    sync

--- cent/mission/gaussian/profile/test_profile.py
+++ cent/mission/gaussian/profile/test_profile.py
@@ -4,7 +4,6 @@ from .mission import Mission
 def test_profile():
     # f = "profile_vanilla_train"
     # f = "profile_vanilla_eval"
-    # f = "profile_vanilla_diff_init_train"
-    f = "profile_vanilla_diff_trainer"
+    f = "profile_vanilla_diff_init_train"
     m = Mission(f + ".json")
     m.exec()
--- cent/mission/gaussian/sparse_profile/test_sparse_profile.py
+++ cent/mission/gaussian/sparse_profile/test_sparse_profile.py
@@ -2,7 +2,8 @@ import pytest
 from .mission import Mission 
 @pytest.mark.current 
 def test_profile():
-    f = "vanilla_basic_diff_init_train"
+    # f = "vanilla_basic_diff_init_train"
     # f = "vanilla_vanilla_diff_init_train"
+    f = "vanilla_diff_trainer_train"
     mission = Mission(f + ".json")
     mission.exec()
