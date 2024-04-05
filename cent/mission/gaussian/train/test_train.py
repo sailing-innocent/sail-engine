65d06f8ba8f470311377bf469fc6ea6de843c21a
sailing-innocent <colored_black@outlook.com>
2024-04-05 12:50:00 +0800

    sync

--- cent/mission/gaussian/train/test_train.py
+++ cent/mission/gaussian/train/test_train.py
@@ -5,9 +5,9 @@ def test_train():
     # f = "train_vanilla_basic"
     # f = "train_vanilla_vanilla"
     # f = "train_vanilla_only_trick"
-    # f = "train_inno_reprod_basic"
+    f = "train_inno_reprod_basic"
     # f = "train_inno_reprod_vanilla"
-    f = "train_inno_torch_basic"
+    # f = "train_inno_torch_basic"
     # f = "train_inno_torch_vanilla"
     mission = Mission(f + ".json")
     mission.exec()
