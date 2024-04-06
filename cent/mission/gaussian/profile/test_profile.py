dcc04902e1dae332ae4fd5489c52c28e28fd02b3
sailing-innocent <colored_black@outlook.com>
2024-04-06 19:13:12 +0800

    feat: test mip360

--- cent/mission/gaussian/profile/test_profile.py
+++ cent/mission/gaussian/profile/test_profile.py
@@ -4,6 +4,7 @@ from .mission import Mission
 def test_profile():
     # f = "profile_vanilla_train"
     # f = "profile_vanilla_eval"
-    f = "profile_vanilla_diff_init_train"
+    # f = "profile_vanilla_diff_init_train"
+    f = "profile_vanilla_diff_tp_train"
     m = Mission(f + ".json")
     m.exec()
