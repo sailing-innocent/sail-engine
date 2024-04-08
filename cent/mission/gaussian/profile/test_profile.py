58dbb22b403e9e1f89b3a710eb89b22b0bf9d72b
sailing-innocent <colored_black@outlook.com>
2024-04-08 09:26:23 +0800

    feat: next compare basic train

--- cent/mission/gaussian/profile/test_profile.py
+++ cent/mission/gaussian/profile/test_profile.py
@@ -7,6 +7,7 @@ def test_profile():
     # f = "profile_vanilla_diff_init_train"
     # f = "profile_vanilla_diff_tp_train"
     # f = "profile_mip360_train"
-    f = "profile_mip360_eval"
+    # f = "profile_mip360_eval"
+    f = "profile_vanilla_diff_train_loss_mip360_train"
     m = Mission(f + ".json")
     m.exec()
