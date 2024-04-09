747d4402add5668fa3818169f5df80171c93da1b
sailing-innocent <colored_black@outlook.com>
2024-04-09 11:22:03 +0800

    feat: depth

--- cent/mission/gaussian/sparse_train/test_train.py
+++ cent/mission/gaussian/sparse_train/test_train.py
@@ -6,7 +6,7 @@ def test_train():
     # f = "train_vanilla_vanilla"
     # f = "train_vanilla_epipolar"
     # f = "train_sparse_vanilla"
-    f = "train_vv_mip360"
-    # f = "train_dd_mip360d"
+    # f = "train_vv_mip360"
+    f = "train_dd_mip360d"
     mission = Mission(f + ".json")
     mission.exec()
