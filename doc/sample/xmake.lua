c9d3f68c4bede58867f80ceae758a4bf969d3389
sailing-innocent <colored_black@outlook.com>
2024-04-15 10:28:35 +0800

    feat: add use tikz and pgf

--- doc/sample/xmake.lua
+++ doc/sample/xmake.lua
@@ -1,11 +1,13 @@
 if (get_config("doc_all")) then 
     includes("use_arxiv")   
-    -- includes("use_si_template") 
-    -- includes("use_cvpr")
-    -- includes("use_njupre")
-    -- includes("use_njuthesis")
-    -- includes("use_eccv")
-    -- includes("aljabr")
-    -- includes("use_acmart")
+    includes("use_si_template") 
+    includes("use_cvpr")
+    includes("use_njupre")
+    includes("use_njuthesis")
+    includes("use_eccv")
+    includes("aljabr")
+    includes("use_acmart")
     includes("use_asset")
 end 
+
+includes("use_tikz")
