60c7a05fced1944d0010cd41c22f005c667216a6
sailing-innocent <colored_black@outlook.com>
2024-04-24 21:46:59 +0800

    sync

---
+++ doc/content/computer_graphics/render/camera/xmake.lua
@@ -0,0 +1,7 @@
+add_content("flip_y_camera_doc_cn", {
+    "fig_flip_y_camera"
+})
+
+add_content("flip_z_camera_doc_cn", {
+    "fig_flip_z_camera"
+})
--- doc/figure/diagram/research/xmake.lua
+++ doc/figure/diagram/research/xmake.lua
@@ -1,3 +1,4 @@
 add_img("fig_research_overview")
 add_img("fig_tile_based_raster")
 add_content("fig_flip_y_camera")
+add_content("fig_flip_z_camera")
