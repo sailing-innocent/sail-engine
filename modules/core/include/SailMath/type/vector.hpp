d7d72212cefb8de8971e2f708ac19f14361a21aa
sailing-innocent <colored_black@outlook.com>
2024-04-29 19:09:35 +0800

    feat: fector

--- modules/core/include/SailMath/type/vector.hpp
+++ modules/core/include/SailMath/type/vector.hpp
@@ -16,10 +16,82 @@ template<ArithemeticT T, size_t N>
 class Vector {
 public:
 	Vector() = default;
+	explicit Vector(T value) {
+		for (size_t i = 0; i < N; ++i) {
+			m_data[i] = value;
+		}
+	}
+	explicit Vector(const eastl::array<T, N>& data) : m_data(data) {}
+	// param list
+	template<typename... Args>
+	explicit Vector(Args... args) : m_data{args...} {
+		static_assert(sizeof...(args) == N, "the size of args should be equal to N");
+	}
+	// copy
+	Vector(const Vector& other) = default;
+	Vector& operator=(const Vector& other) = default;
+	// move
+	Vector(Vector&& other) noexcept = default;
+	Vector& operator=(Vector&& other) noexcept = default;
 
-	// getter
-	[[nodiscard]] T& operator[](size_t index) const noexcept {
+	[[nodiscard]] T& operator[](size_t index) noexcept {
 		return m_data[index];
+	}
+	[[nodiscard]] const T& operator[](size_t index) const noexcept {
+		return m_data[index];
+	}
+
+	// operator
+	friend Vector operator+(const Vector& lhs, const Vector& rhs) {
+		Vector result;
+		for (size_t i = 0; i < N; ++i) {
+			result[i] = lhs[i] + rhs[i];
+		}
+		return result;
+	}
+	friend Vector operator-(const Vector& lhs, const Vector& rhs) {
+		Vector result;
+		for (size_t i = 0; i < N; ++i) {
+			result[i] = lhs[i] - rhs[i];
+		}
+		return result;
+	}
+	// scalar multiply
+	friend Vector operator*(const Vector& lhs, T rhs) {
+		Vector result;
+		for (size_t i = 0; i < N; ++i) {
+			result[i] = lhs[i] * rhs;
+		}
+		return result;
+	}
+	friend Vector operator*(T lhs, const Vector& rhs) {
+		Vector result;
+		for (size_t i = 0; i < N; ++i) {
+			result[i] = lhs * rhs[i];
+		}
+		return result;
+	}
+	friend Vector operator/(const Vector& lhs, T rhs) {
+		Vector result;
+		for (size_t i = 0; i < N; ++i) {
+			result[i] = lhs[i] / rhs;
+		}
+		return result;
+	}
+	// dot product
+	friend T dot(const Vector& lhs, const Vector& rhs) {
+		T result = 0;
+		for (size_t i = 0; i < N; ++i) {
+			result += lhs[i] * rhs[i];
+		}
+		return result;
+	}
+	// cross product
+	friend Vector cross(const Vector& lhs, const Vector& rhs) {
+		static_assert(N == 3, "cross product is only defined for 3D vector");
+		return Vector(lhs[1] * rhs[2] - lhs[2] * rhs[1],
+					  lhs[2] * rhs[0] - lhs[0] * rhs[2],
+					  lhs[0] * rhs[1] - lhs[1] * rhs[0]);
 	}
 
 private:
--- tests/core/math/test_vector.cpp
+++ tests/core/math/test_vector.cpp
@@ -1,11 +1,52 @@
 #include "test_util.h"
 #include "SailMath/type/vector.hpp"
+#include <EASTL/array.h>
+#include <EASTL/numeric.h>
 
 namespace sail::test {
 
 template<typename T, size_t N>
 int test_vector() {
-	Vector<T, N> v;
+	eastl::array<T, N> data1;
+	eastl::iota(data1.begin(), data1.end(), 0);
+	eastl::array<T, N> data2;
+	eastl::iota(data2.begin(), data2.end(), 1);
+
+	Vector<T, N> v1(data1);
+	Vector<T, N> v2(data2);
+	// add
+	Vector<T, N> v3 = v1 + v2;
+	for (size_t i = 0; i < N; ++i) {
+		CHECK(v3[i] == doctest::Approx(data1[i] + data2[i]));
+	}
+	// minus
+	v3 = v1 - v2;
+	for (size_t i = 0; i < N; ++i) {
+		CHECK(v3[i] == doctest::Approx(data1[i] - data2[i]));
+	}
+	// scalar multiply
+	v3 = v1 * 2;
+	for (size_t i = 0; i < N; ++i) {
+		CHECK(v3[i] == doctest::Approx(data1[i] * 2));
+	}
+	// scalar divide
+	v3 = v1 / 2;
+	for (size_t i = 0; i < N; ++i) {
+		CHECK(v3[i] == doctest::Approx(data1[i] / 2));
+	}
+	// dot product
+	T result = dot(v1, v2);
+	T expect = eastl::inner_product(data1.begin(), data1.end(), data2.begin(), static_cast<T>(0));
+	CHECK(result == doctest::Approx(expect));
+
+	// cross product
+	if constexpr (N == 3) {
+		Vector<T, N> v4 = cross(v1, v2);
+		CHECK(v4[0] == doctest::Approx(data1[1] * data2[2] - data1[2] * data2[1]));
+		CHECK(v4[1] == doctest::Approx(data1[2] * data2[0] - data1[0] * data2[2]));
+		CHECK(v4[2] == doctest::Approx(data1[0] * data2[1] - data1[1] * data2[0]));
+	}
+
 	return 0;
 }
 
