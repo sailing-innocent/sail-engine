ca98081a8f19a48613831a1cad86c1b6c6cf9006
sailing-innocent <colored_black@outlook.com>
2024-04-29 20:10:48 +0800

    feat: my transform class framework

--- modules/core/include/SailMath/type/transform.hpp
+++ modules/core/include/SailMath/type/transform.hpp
@@ -1,40 +1,90 @@
+#pragma once
+/**
+ * @file transform.hpp
+ * @brief The Transform
+ * @author sailing-innocent
+ * @date 2024-04-29
+ */
 // 3D Geometry Transform
 #include <EASTL/vector.h>
+#include <EASTL/unique_ptr.h>
+#include "SailMath/type/matrix.hpp"
+#include "SailMath/type/vector.hpp"
+#include "SailMath/type/quaternion.hpp"
 
 namespace sail {
 
+template<int DIM>
 class Transform {
+	bool is_homogeneous = false;
+	bool m_mat_cached = false;
+
 public:
 	Transform();
 	virtual ~Transform();
 
+	// Get the matrix of the transform
+	Matrix<float, DIM, DIM>& matrix();
+	// operator *: transform * transform
+	Transform<DIM> operator*(const Transform<DIM>& rhs) const;
+
 protected:
 	eastl::vector<Transform> m_sub_transform;
+	eastl::unique_ptr<
+		Matrix<float, DIM, DIM>>
+		mp_mat = nullptr;// matrix to be cached
+	eastl::unique_ptr<
+		Matrix<float, DIM + 1, DIM + 1>>
+		mp_homo_mat = nullptr;// matrix of homogeneous coord
 };
 
-class Rotation : public Transform {
+template<int DIM>
+class ScaleTransform : public Transform<DIM> {
 public:
-	Rotation();
-	~Rotation();
+	explicit ScaleTransform(float scale);
+	explicit ScaleTransform(const Vector<float, DIM>& scale);
+	virtual ~ScaleTransform();
 
-	// Rotation Display
-	enum class RotationType : int {
-		Matrix,
-		Quaternion,
-		AxisAngle
+private:
+	Vector<float, DIM> m_scale;
+};
+
+template<int DIM>
+class TranslateTransform : public Transform<DIM> {
+public:
+	explicit TranslateTransform(const Vector<float, DIM>& translate);
+	virtual ~TranslateTransform();
+
+private:
+	Vector<float, DIM> m_translate;
+};
+
+// variant of RotationConstruct Data
+
+template<int DIM>
+class RotateTransform : public Transform<DIM> {
+	bool m_axis_angle_cached = false;
+	bool m_euler_angle_cached = false;
+	bool m_quaternion_cached = false;
+
+public:
+	explicit RotateTransform(float angle);
+	explicit RotateTransform(const Vector<float, DIM>& axis, float angle);
+	explicit RotateTransform(const Vector<float, DIM>& euler_angle);
+	explicit RotateTransform(const Quaternion<float>& quaternion);
+
+	virtual ~RotateTransform();
+
+	enum class RotateType : int {
+		AXIS_ANGLE,
+		EULER_ANGLE,
+		QUATERNION
 	};
-};
 
-class Translation : public Transform {
-public:
-	Translation();
-	~Translation();
-};
-
-class ViewTransform : public Transform {
-public:
-	ViewTransform();
-	~ViewTransform();
+	// Rotation Type Transform
+	RotateTransform to_axis_angle(bool cached = false);
+	RotateTransform to_euler_angle(bool cached = false);
+	RotateTransform to_quaternion(bool cached = false);
 };
 
 }// namespace sail
