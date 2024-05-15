37e5f566bbd15554e47ce1a7700ec6fb95c78a86
sailing-innocent <colored_black@outlook.com>
2024-05-16 00:20:48 +0800

    i hate this

---
+++ modules/compute/include/SailCompute/core/constant.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace sail {
+
+constexpr float PI_f = 3.14159265358979323846f;
+constexpr double PI_d = 3.14159265358979323846;
+constexpr float PI_OVER_TWO_f = 1.57079632679489661923f;
+constexpr double PI_OVER_TWO_d = 1.57079632679489661923;
+constexpr float PI_OVER_FOUR_f = 0.785398163397448309616f;
+constexpr double PI_OVER_FOUR_d = 0.785398163397448309616;
+constexpr float INV_PI_f = 0.318309886183790671538f;
+constexpr double INV_PI_d = 0.318309886183790671538;
+constexpr float SQRT_2_f = 1.41421356237309504880f;
+constexpr double SQRT_2_d = 1.41421356237309504880;
+constexpr float INV_SQRT_2_f = 0.707106781186547524401f;
+constexpr double INV_SQRT_2_d = 0.707106781186547524401;
+// 1 - eps
+constexpr float ONE_MINUS_EPS_f = 0x1.fffffep-1f;
+constexpr double ONE_MINUS_EPS_d = 0x1.fffffffffffffp-1;
+
+}// namespace sail
