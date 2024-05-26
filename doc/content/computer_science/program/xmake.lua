9006e97570b5e76507d6f2ce7e7b6dbc859724cf
sailing-innocent <colored_black@outlook.com>
2024-05-26 15:46:55 +0800

    feat: update cn note template

--- doc/content/computer_science/program/xmake.lua
+++ doc/content/computer_science/program/xmake.lua
@@ -3,7 +3,6 @@ target("fig_ast_example")
     add_files("ast_example.gv")
 target_end()
 
-
 add_content("abstract_semantic_tree_brief_doc_cn", {
     "fig_ast_example"
 })
--- doc/sample/use_si_template/xmake.lua
+++ doc/sample/use_si_template/xmake.lua
@@ -1,2 +1,2 @@
-add_latex("use_si_cn_note", {"si_template"}, "cn_note")
+add_latex("use_si_cn_book", {"si_template"}, "cn_book")
 add_latex("use_si_cn_pre", {"si_template"}, "cn_pre")
