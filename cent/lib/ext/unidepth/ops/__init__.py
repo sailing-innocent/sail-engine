263c6a076fbd81064cb8ec059365050d28f8ee09
sailing-innocent <colored_black@outlook.com>
2024-06-19 08:58:18 +0800

    remove cpp module

--- cent/lib/ext/unidepth/ops/__init__.py
+++
@@ -1,9 +0,0 @@
-from .losses import SILog, MSE, SelfCons
-from .scheduler import CosineScheduler
-
-__all__ = [
-    "SILog",
-    "MSE",
-    "SelfCons",
-    "CosineScheduler",
-]
--- cent/lib/reimpl/vanilla_diff_gaussian/test/test_diff_gaussian.py
+++
@@ -1,5 +0,0 @@
-import pytest 
-
-@pytest.mark.app
-def test_diff_gaussian():
-    assert True 
